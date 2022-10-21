3803953281701589ee86b4747c5ba94516ce035c
Akash <akash.kushwaha02@gmail.com>
2022-10-21 11:44:57 +0530

    'without_stripe'

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,11 +6,31 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+    console.log("before axio");
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: "akash.kushwaha@gmail.com",
+    });
+    console.log("after axio");
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
+
   console.log(session, "session ");
   return (
     <div className="bg-gray-100">
@@ -62,7 +82,9 @@ function Checkout() {
                 <Currency quantity={total} currency={"INR"} />
               </span>
               <button
+                role="link"
                 disabled={!session}
+                onClick={createCheckoutSession}
                 className={`button mt-2 ${
                   !session &&
                   "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
